4178569ed0f6f6c078812103951aac3dbd6177df
atmmmmt <salemalnajjar7@gmail.com>
2026-09-26 19:22:14 +0300

    feat: expose shareable VAYRO live demo route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { LiveDemo } from './LiveDemo';
+import { LiveDemoLauncher } from './LiveDemoLauncher';
 import './i18n';
 import './styles.css';
 import './premium-landing-v2.css';
 import './premium-landing-v3.css';
 
+const isLiveDemo = /^\/(ar|en|tr|fr)\/demo\/?$/.test(window.location.pathname);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isLiveDemo ? (
+      <BrowserRouter>
+        <LiveDemo />
+      </BrowserRouter>
+    ) : (
+      <>
+        <App />
+        <LiveDemoLauncher />
+      </>
+    )}
   </React.StrictMode>,
 );
